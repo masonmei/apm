8b499a3686038dac725ff5f916d415974f868751
yangbl928 <570732443@qq.com>
2015-08-13 16:31:14 +0800

    封装异常和时间处理工具类

--- commons/src/main/java/com/baidu/oped/apm/common/util/TimeUtils.java
+++ commons/src/main/java/com/baidu/oped/apm/common/util/TimeUtils.java
@@ -1,8 +1,12 @@
 
 package com.baidu.oped.apm.common.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
- * class TimeUtils 
+ * class TimeUtils
  *
  * @author meidongxu@baidu.com
  */
@@ -21,4 +25,39 @@ public final class TimeUtils {
     public static long recoveryTimeMillis(long reverseCurrentTimeMillis) {
         return Long.MAX_VALUE - reverseCurrentTimeMillis;
     }
+
+    public static String formatTime(long timeMillis) {
+        Date now = new Date(timeMillis);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(now);
+    }
+
+    public static String formatTime(long timeMillis, String formatStr) {
+        Date now = new Date(timeMillis);
+        SimpleDateFormat format = new SimpleDateFormat(formatStr);
+        return format.format(now);
+    }
+
+    public static long getTimeStampL(String timeStr) throws Exception {
+        return getTimeStampL(timeStr, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    public static long getTimeStampL(String timeStr, String format) throws Exception {
+        DateFormat dateformat = new SimpleDateFormat(format);
+        Date date = dateformat.parse(timeStr);
+        return date.getTime();
+    }
+
+    public static int getTimeStampI(String timeStr) throws Exception {
+        return getTimeStampI(timeStr, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    public static int getTimeStampI(String timeStr, String format) throws Exception {
+        return (int) (getTimeStampL(timeStr, format) / 1000);
+    }
+
+    public static String getDay(long timestamp) {
+        return formatTime(timestamp, "yyyyMMdd");
+    }
+
 }
