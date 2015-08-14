2f9f95660370a66ea67ea18fb51d4e273d3a28c5
Mason <dongxu.m@gmail.com>
2015-08-14 16:15:00 +0800

    change hbase server

--- web/src/main/java/com/baidu/oped/apm/config/filter/CORSFilter.java
+++ web/src/main/java/com/baidu/oped/apm/config/filter/CORSFilter.java
@@ -23,10 +23,12 @@ public class CORSFilter implements Filter {
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("HTTP_ORIGIN"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "86400");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
         chain.doFilter(req, res);
     }
 
