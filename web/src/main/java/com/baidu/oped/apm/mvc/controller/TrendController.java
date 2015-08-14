445449e6118f538af87e5d6c60a2be5613eca6a3
yangbl928 <570732443@qq.com>
2015-08-14 15:02:08 +0800

    趋势图查询

--- web/src/main/java/com/baidu/oped/apm/mvc/controller/TrendController.java
+++ web/src/main/java/com/baidu/oped/apm/mvc/controller/TrendController.java
@@ -1,7 +1,6 @@
 package com.baidu.oped.apm.mvc.controller;
 
 import com.baidu.oped.apm.common.Exception.ParameterException;
-import com.baidu.oped.apm.common.util.TimeUtils;
 import com.baidu.oped.apm.model.service.TraceIndexService;
 import com.baidu.oped.apm.model.service.TransactionService;
 import com.baidu.oped.apm.mvc.vo.BusinessTransactions;
@@ -10,10 +9,14 @@ import com.baidu.oped.apm.mvc.vo.Range;
 import com.baidu.oped.apm.mvc.vo.TransactionId;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,31 +37,28 @@ public class TrendController {
     public DataTrend overview(
             @RequestParam(value = "appName", required = true) String applicationName,
             @RequestParam(value = "agentId", required = false, defaultValue = "") String agentId,
-            @RequestParam(value = "startTime", required = true) String startTime,
-            @RequestParam(value = "endTime", required = true) String endTime,
+            @RequestParam(value = "from", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime from,
+            @RequestParam(value = "to", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime to,
             @RequestParam(value = "period", required = true) int period,
             @RequestParam(value = "contrast", required = false, defaultValue = "false") boolean contrast
     ) throws Exception {
-        System.out.println("startTime:" + startTime + ", endTime:" + endTime);
-        long start, end;
-        try {
-            if (period < 60) {
-                throw new ParameterException("period must more than 60");
-            }
-            period = period / 60 * 60; // 规整period
-            startTime = "2015-08-14 10:00:00";
-            endTime = "2015-08-14 11:00:00";
-            start = TimeUtils.getTimeStampL(startTime);
-            end = TimeUtils.getTimeStampL(endTime);
-            start = 1439517600000L;
-            end = 1439521200000L;
-        }catch (Exception e) {
-            throw new ParameterException(e);
+        if (period < 60) {
+            throw new ParameterException("period must more than 60");
         }
 
-        System.out.println("start:" + start + ", end:" + end + ", appName:" + applicationName + ", period:" + period);
+        if (from == null) {
+            from = LocalDateTime.now().plus(Duration.ofMinutes(-5));
+        }
 
-        Range range = new Range(start, end);
+        if (to == null || to.isBefore(from)) {
+            to = LocalDateTime.now();
+        }
+
+        Range range = new Range(from.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(),
+                to.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+
         List<TransactionId> transactionIds = traceIndexService.selectTraceIds(applicationName, range);
 
         List<TransactionId> neededTransactionIds = new ArrayList<TransactionId>();
