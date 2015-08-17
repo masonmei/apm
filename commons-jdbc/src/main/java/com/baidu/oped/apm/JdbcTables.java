b9c48250bf3bcfa63472638aab6598566fa2b854
Mason <dongxu.m@gmail.com>
2015-08-17 16:11:33 +0800

    Remote Hbase and will switch to jdbc

---
+++ commons-jdbc/src/main/java/com/baidu/oped/apm/JdbcTables.java
@@ -0,0 +1,18 @@
+package com.baidu.oped.apm;
+
+/**
+ * Created by mason on 8/17/15.
+ */
+public interface JdbcTables {
+    String TRACE = "apm_span";
+    String TRACE_EVENT = "apm_span_event";
+    String AGENT_CPU_LOAD = "apm_agent_cpu_load";
+    String AGENT_INFO = "apm_agent_info";
+    String AGENT_STAT_MEMORY_GC = "apm_agent_stat_memory_gc";
+    String ANNOTATION = "apm_annotation";
+    String API_META_DATA = "apm_api_meta_data";
+    String SERVER_META_DATA = "apm_server_meta_data";
+    String SERVER_INFO = "apm_service_info";
+    String SQL_META_DATA = "apm_sql_meta_data";
+    String STRING_META_DATA = "apm_string_meta_data";
+}
