f8fecd216ea9421d196449721ccb98d9621be3dd
yangbl928 <570732443@qq.com>
2015-08-17 20:42:31 +0800

    封装异常和时间处理工具类

--- commons/src/main/java/com/baidu/oped/apm/common/util/ClassUtils.java
+++ commons/src/main/java/com/baidu/oped/apm/common/util/ClassUtils.java
@@ -1,6 +1,10 @@
 
 package com.baidu.oped.apm.common.util;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * class ClassUtils 
  *
@@ -47,4 +51,50 @@ public final class ClassUtils {
         }
         return fqcn.substring(0, lastPackageSeparatorIndex);
     }
+
+    /**
+     * 获取类实例的属性值(如果父类和子类有相同的属性，则只取子类)
+     *
+     * @param clazz
+     *            类名
+     * @param includeParentClass
+     *            是否包括父类的属性值
+     * @return 属性名=属性
+     */
+    public static Map<String, Field> getClassFields(Class<?> clazz, boolean includeParentClass) {
+        Map<String, Field> map = new HashMap<String, Field>();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            map.put(field.getName(), field);
+        }
+        if (includeParentClass)
+            getParentClassFields(map, clazz.getSuperclass());
+        return map;
+    }
+
+    /**
+     * 获取类实例的父类的属性值
+     *
+     * @param map
+     *            类实例的属性值Map
+     * @param clazz
+     *            类名
+     * @return 属性名=属性
+     */
+    private static void getParentClassFields(Map<String, Field> map, Class<?> clazz) {
+        if (clazz == null) {
+            return;
+        }
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (!map.containsKey(field.getName())) {
+                map.put(field.getName(), field);
+            }
+        }
+        if (clazz.getSuperclass() == null) {
+            return;
+        }
+        getParentClassFields(map, clazz.getSuperclass());
+    }
+
 }
