453711148e7be89ff57a39d3ac80cfc82ddbe2a5
Mason <dongxu.m@gmail.com>
2015-08-20 19:33:03 +0800

    reexport data initial scripts

--- web/src/main/java/com/baidu/oped/apm/model/dao/ApplicationTraceIndexDao.java
+++ web/src/main/java/com/baidu/oped/apm/model/dao/ApplicationTraceIndexDao.java
@@ -1,36 +1,16 @@
-//package com.baidu.oped.apm.model.dao;
-//
-//import java.util.List;
-//
-//import com.baidu.oped.apm.mvc.vo.Range;
-//import com.baidu.oped.apm.mvc.vo.TransactionId;
-//
-///**
-// * class ApplicationTraceIndexDao
-// *
-// * @author meidongxu@baidu.com
-// */
-//public interface ApplicationTraceIndexDao {
-//
-//    List<TransactionId> scanTraceIndex(String applicationName, Range range, int limit);
-//
-//    //    LimitedScanResult<List<TransactionId>> scanTraceIndex(String applicationName, SelectedScatterArea range,
-//    // int limit);
-//
-//    //    List<Dot> scanTraceScatter(String applicationName, Range range, int limit);
-//    //
-//    //    /**
-//    //     *
-//    //     * select transactions in a selection range(box) in the scatter chart.
-//    //     *
-//         * @param applicationName
-//         * @param area
-//    //     * @param offsetTransactionId
-//    //     * @param offsetTransactionElapsed
-//    //     * @param limit
-//    //     * @return
-//    //     */
-//    //    List<Dot> scanTraceScatter(String applicationName, SelectedScatterArea area, TransactionId
-//    // offsetTransactionId,
-//    //                               int offsetTransactionElapsed, int limit);
-//}
+package com.baidu.oped.apm.model.dao;
+
+import java.util.List;
+
+import com.baidu.oped.apm.mvc.vo.Range;
+import com.baidu.oped.apm.mvc.vo.TransactionId;
+
+/**
+ * class ApplicationTraceIndexDao
+ *
+ * @author meidongxu@baidu.com
+ */
+public interface ApplicationTraceIndexDao {
+
+    List<TransactionId> scanTraceIndex(String applicationName, Range range, int limit);
+}
--- web/src/main/java/com/baidu/oped/apm/model/service/TraceIndexService.java
+++ web/src/main/java/com/baidu/oped/apm/model/service/TraceIndexService.java
@@ -1,30 +1,30 @@
-//package com.baidu.oped.apm.model.service;
-//
-//import java.util.List;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.util.Assert;
-//
-//import com.baidu.oped.apm.model.dao.ApplicationTraceIndexDao;
-//import com.baidu.oped.apm.mvc.vo.Range;
-//import com.baidu.oped.apm.mvc.vo.TransactionId;
-//
-///**
-// * Created by mason on 8/13/15.
-// */
-//@Service
-//public class TraceIndexService {
-//    private static final Logger LOG = LoggerFactory.getLogger(TraceIndexService.class);
-//
-//    @Autowired
-//    private ApplicationTraceIndexDao traceIndexDao;
-//
-//    public List<TransactionId> selectTraceIds(String applicationName, Range range) {
-//        Assert.hasLength(applicationName, "ApplicationName cannot be null while selecting trace ids.");
-//        Assert.notNull(range, "Range cannot be null while selecting trace ids.");
-//        return traceIndexDao.scanTraceIndex(applicationName, range, -1);
-//    }
-//}
+package com.baidu.oped.apm.model.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.baidu.oped.apm.model.dao.ApplicationTraceIndexDao;
+import com.baidu.oped.apm.mvc.vo.Range;
+import com.baidu.oped.apm.mvc.vo.TransactionId;
+
+/**
+ * Created by mason on 8/13/15.
+ */
+@Service
+public class TraceIndexService {
+    private static final Logger LOG = LoggerFactory.getLogger(TraceIndexService.class);
+
+    @Autowired
+    private ApplicationTraceIndexDao traceIndexDao;
+
+    public List<TransactionId> selectTraceIds(String applicationName, Range range) {
+        Assert.hasLength(applicationName, "ApplicationName cannot be null while selecting trace ids.");
+        Assert.notNull(range, "Range cannot be null while selecting trace ids.");
+        return traceIndexDao.scanTraceIndex(applicationName, range, -1);
+    }
+}
