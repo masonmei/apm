50d92e405997e21d32a5336a80268cd7c6c32746
杨博淋 <570732443@qq.com>
2015-08-27 21:27:24 +0800

    page  util

---
+++ web/src/main/java/com/baidu/oped/apm/utils/PageUtils.java
@@ -0,0 +1,27 @@
+package com.baidu.oped.apm.utils;
+
+import com.sun.tools.javac.util.Pair;
+import org.springframework.util.StringUtils;
+
+/**
+ * 分页工具类
+ * Created with IntelliJ IDEA.
+ * User: yangbolin
+ * Date: 15/8/27
+ * Time: 21:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class PageUtils {
+
+    public static Pair<Boolean, String> parseOrder(String orderBy) {
+        if (StringUtils.isEmpty(orderBy)) {
+            return null;
+        }
+        boolean order = true;
+        if (orderBy.startsWith("-")) {
+            order = false;
+        }
+        Pair<Boolean, String> pair = new Pair<>(order, orderBy.substring(1));
+    }
+
+}
